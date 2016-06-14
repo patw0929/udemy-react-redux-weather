24218453f5aad72a47e08391bc4d8c2adb7583c7
patw <patw.hi@gmail.com>
2016-06-14 12:39:08 +0800

    Use axios to retrieve weather data in promise object

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
 import { API_KEY } from '../config/config';
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city}`;
+
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
