1a0498734483dca0efa9f4b5b7a0a4e09b749e8b
patw <patw.hi@gmail.com>
2016-06-14 13:23:10 +0800

    Set weather api's units as metric

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { API_KEY } from '../config/config';
 
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=metric`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
