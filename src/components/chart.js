95ba837225598a3c0bc6040289869a794eeaf7d4
patw <patw.hi@gmail.com>
2016-06-14 13:23:55 +0800

    Added average and reference line in chart

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,12 +1,19 @@
+import _ from 'lodash';
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+
+function average(data) {
+  return (_.sum(data)/data.length);
+}
 
 export default (props) => {
   return (
     <div>
       <Sparklines data={props.data} width={180} height={120}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>{average(props.data)}</div>
     </div>
   );
 };
